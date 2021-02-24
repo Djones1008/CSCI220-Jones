78db26b8bd4326b3fdf3fe4047ff01acc3e16950
Djones1008 <JonesDD1@cougars.sf.edu>
2021-02-24 17:11:10 -0500

    In class stuff

---
+++ week7/fizzbuzz.js
@@ -0,0 +1,16 @@
+let counter = 0;
+while(counter<=100)
+{
+
+  if(counter)
+    {
+        console.log("Fizz")
+    }
+    else
+    {
+        console.log(counter)
+        counter++; 
+    }
+
+    
+}
